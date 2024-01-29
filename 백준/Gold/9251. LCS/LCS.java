78d4240dcc730b58b7cb6b43131b73784b0bdb7a
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2024-01-29 23:32:48 +0900

    [Gold V] Title: LCS, Time: 152 ms, Memory: 18540 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine(), str2 = br.readLine();
+        int N = str1.length(), M = str2.length();
+
+        int[][] dp = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[N][M]);
+    }
+}
+/*
+CCAYKP
+CAPCAK
+1
+1  2
+*/
