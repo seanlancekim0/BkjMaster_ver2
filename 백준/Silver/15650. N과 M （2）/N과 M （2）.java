115b2547439b1cbd8dbdc03e3cd1ca617ca0f8e4
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2024-02-19 01:54:57 +0900

    [Silver III] Title: N과 M (2), Time: 156 ms, Memory: 16364 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -23,11 +23,15 @@ public class Main {
     }
 
     private static void add(int s, int l, String str) {
+        //무사히 길이가 M인 문자열이 만들어졌을 때 sb에 추가
         if (l == M){
             sb.append(str).append("\n");
             return;
         }
+        //길이가 M이 되기 전에 자연수가 N을 초과하는 경우
         if (++s > N) return;
+        
+        //아직 완성되지 못한 수열들을 재귀로 계속 진행
         for (int i = s; i <= N; i++) {
             add(i, l + 1, str + " " + i);
         }
