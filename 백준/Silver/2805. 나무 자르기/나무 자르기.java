b32e9f40cc024df277fe0b89f33cb8169aa0823b
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2024-06-24 16:53:48 +0900

    [Silver II] Title: 나무 자르기, Time: 776 ms, Memory: 145372 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,86 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int mid, M, N, min, max;
+    static ArrayList<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+
+        int temp = Integer.parseInt(st.nextToken());
+//        min = temp;
+        min = 0;
+        max = temp;
+        list.add(temp);
+
+        for (int i = 1; i < N; i++) {
+            temp = Integer.parseInt(st.nextToken());
+//            min = Math.min(temp, min);
+            max = Math.max(temp, max);
+            list.add(temp);
+//            System.out.println("min: " + min + " max: " + max);
+        }
+//        System.out.println();
+
+        BSearchIter();
+        System.out.print(mid);
+
+    }
+
+    private static void BSearchRec() {
+        System.out.println("min: " + min + " max: " + max);
+        long result = 0;
+        mid = (min + max) / 2;
+        System.out.println("mid: "+mid);
+        for (int a: list) {
+            int diff = a - mid;
+            if (diff > 0) result += diff;
+        }
+        System.out.println("result: " + result);
+        System.out.println();
+        if (result == M) return;
+        if (result > M) {
+            min = mid + 1;
+            if (min > max) return;
+        } else {
+            max = mid - 1;
+        }
+        BSearchRec();
+    }
+
+    private static void BSearchIter() {
+        long result = 0;
+
+        // 수정된 부분: 재귀 호출 대신 반복문을 사용하여 이진 탐색을 수행
+        while (min <= max) {
+            mid = (min + max) / 2;
+            result = 0;
+
+            for (int a : list) {
+                int diff = a - mid;
+                if (diff > 0) result += diff;
+            }
+
+            if (result == M) return;
+            else if (result > M) {
+                min = mid + 1;
+//                if (min == max) {
+//                    break;
+//                }
+            } else {
+                max = mid - 1;
+            }
+        }
+        if (result < M) mid--;
+    }
+}
