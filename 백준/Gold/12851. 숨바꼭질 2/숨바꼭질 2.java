5b42749239de4f5acdc6e34fdcc690a1851a11f4
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2025-01-14 13:39:10 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 160 ms, Memory: 37736 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,7 +1,7 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -9,8 +9,8 @@ import java.util.StringTokenizer;
 public class Main {
     // 0 ≤ N, K ≤ 100,000
     static int N, K, count;
-
     static Queue<State> queue;
+    // 각 지점의 최소시간을 저장하는 배열
     static int[] minTime;
 
     public static void main(String[] args) throws IOException {
@@ -29,16 +29,17 @@ public class Main {
     private static void bfs() {
         while (!queue.isEmpty()) {
             State p = queue.poll();
-//            System.out.println(p.time + " " + p.x);
+
+            // [총 최소시간]보다 [현재 State의 소요시간]이 큰 경우 break
+            if (minTime[K] < p.time) break;
             if (!isValid(p)) continue;
 
-//            System.out.println("\t\t\t" + p.time + " " + p.x);
-
+            // minTime 배열에 해당 지점의 최소시간 저장
             minTime[p.x] = p.time;
 
+            // 목표 지점에 도달했을 때 count 증가
             if (p.x == K) {
                 count++;
-//                System.out.println("------------" + p.time + " " + p.x);
                 continue;
             }
 
@@ -48,11 +49,14 @@ public class Main {
         }
     }
 
+    // 유효 판별 메소드
     private static boolean isValid(State p) {
-        if (p.x < 0 || p.x > 100000 || minTime[K] < p.time || minTime[p.x] < p.time) return false;
+        // [해당 지점의 최소시간]보다 [현재 State의 소요시간]이 큰 경우 invalid함
+        if (p.x < 0 || p.x > 100000 || minTime[p.x] < p.time) return false;
         return true;
     }
 
+    // 초기화 메소드
     private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -60,15 +64,18 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
         count = 0;
         queue = new LinkedList<>();
-        minTime = new int[100001];
-        for (int i = 0; i < 100001; i++) {
-            minTime[i] = Integer.MAX_VALUE;
-        }
         queue.add(new State(N, 0));
+
+        // minTime 배열은 MAX 값으로 초기화
+        minTime = new int[100001];
+        Arrays.fill(minTime, Integer.MAX_VALUE);
     }
 
+    // Queue에 저장할 상태 Class
     private static class State {
+        // 좌표
         int x;
+        // 소요시간
         int time;
 
         public State(int x, int time) {
