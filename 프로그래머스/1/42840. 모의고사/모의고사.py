33922561ad1c4489d8e9478b34f5d2d03714d755
김시현 <122666778+seanlancekim0@users.noreply.github.com>
2025-07-14 20:50:15 +0900

    [level 1] Title: 모의고사, Time: 5.12 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -6,17 +6,14 @@ def solution(answers):
         [3,3,1,1,2,2,4,4,5,5]
     ]
     count = [0, 0, 0]
-    # for i in range(len(answers)):
+    
     for i, ans in enumerate(answers):
-        # for j in range(len(list)):
         for j, lst in enumerate(list):
-            # if list[j][i%len(list[j])] == ans:
             if lst[i % len(lst)] == ans:
                 count[j] += 1
     
-    m = max(count)
     for i, key in enumerate(count):
-        if key == m:
-            answer.append(i+1)
+        if key == max(count):
+            answer.append(i + 1)
     
     return answer
